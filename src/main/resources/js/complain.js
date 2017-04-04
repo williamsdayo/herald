01bf6c36478fcf7ad08447bc1c8182676cfaf711
Abimbola Esuruoso <abimbolaesuruoso@gmail.com>
2017-04-04 13:50:54 +0100

    Added verification to the submit a complaint page

--- src/main/resources/js/complain.js
+++ src/main/resources/js/complain.js
@@ -15,6 +15,11 @@ function complain() {
         headers: { 'Content-Type': 'application/json' }
     });
 
-    client.post('complaints', complaint)
-        .then((_) => window.location.href = "complaints.html");
+    if (title.length > 0 && content.length > 0) {
+
+        client.post('complaints', complaint)
+            .then((_) => window.location.href = "complaints.html");
+    } else {
+        alert("You need to fill in the form before you submit")
+    }
 }
